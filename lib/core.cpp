6aca4851c50f1bd5a4fe48709b744b76cbf8b9cd
ganatcs <ganatcs@example.com>
2013-10-27 14:47:48 +0900

    improve taat

--- lib/core.cpp
+++ lib/core.cpp
@@ -35,40 +35,57 @@ private:
 
 PostingList::PostingList(const std::vector<int>& v): v_(v), i_(v_.begin()) {}
 
+void taat_intersect(PostingList& in,
+		    const std::vector<std::pair<int, int>>& accumlator,
+		    std::vector<std::pair<int, int>>& next_accumlator) {
+  in.init();
+  auto i = in.next();
+  auto j = accumlator.begin();
+  while (i != -1 && j != accumlator.end()) {
+    if (i < j->first) {
+      next_accumlator.push_back(std::make_pair(i, 1));
+      i = in.next();
+    } else if(i == j->first) {
+      next_accumlator.push_back(std::make_pair(j->first, j->second + 1));
+      i = in.next();
+      ++j;
+    } else {
+      next_accumlator.push_back(*j);
+      ++j;
+    }
+  }
+  while (i != -1) {
+    next_accumlator.push_back(std::make_pair(i, 1));
+    i = in.next();
+  }
+  while (j != accumlator.end()) {
+    next_accumlator.push_back(*j);
+    ++j;
+  }
+}
 
 bool taat(std::vector<PostingList> &posting_lists,
+	  const int top_k,
 	  std::vector<std::pair<int, int>>& result) {
   result.clear();
-  std::map<int, int> accumulator;
+
   for (auto posting_list = posting_lists.begin();
        posting_list != posting_lists.end();
        ++posting_list) {
     posting_list->init();
-    while (true) {
-      int tmp_docid = posting_list->next();
-      if (tmp_docid == -1) break;
-      accumulator[tmp_docid]++;
-    }
-  }
-  std::vector<std::pair<int, int>> tmp_v;
+    std::vector<std::pair<int, int>> next_accumlator;
 
-  for (auto i = accumulator.begin();
-       i != accumulator.end();
-       ++i) {
-    tmp_v.push_back(std::make_pair(i->first, i->second));
+    taat_intersect(*posting_list, result, next_accumlator);
+    next_accumlator.swap(result);
   }
-  std::sort(tmp_v.begin(),
-	    tmp_v.end(),
-	    [](const std::pair<int, int>& a, const std::pair<int, int>& b) -> bool{
-	      if (a.second != b.second) return b.second - a.second < 0;
-	      return b.first - a.first > 0;
-	    });
 
-  for (auto i = tmp_v.begin();
-       i != tmp_v.end();
-       ++i) {
-    result.push_back(std::make_pair(i->first, i->second));
-  }
+  std::partial_sort(result.begin(),
+		     result.begin() + top_k,
+		     result.end(),
+		     [](const std::pair<int, int>& a, const std::pair<int, int>& b) -> bool{
+		       if (a.second != b.second) return b.second - a.second < 0;
+		       return b.first - a.first > 0;
+		     });
   return true;
 }
 
@@ -78,7 +95,7 @@ int main() {
   ps.push_back(PostingList({1, 3, 5}));
 
   std::vector<std::pair<int, int>> result;
-  taat(ps, result);
+  taat(ps, 5, result);
 
   for (auto i = result.begin(); i != result.end(); ++i) {
     std::cout << i->first << " " << i->second << std::endl;
